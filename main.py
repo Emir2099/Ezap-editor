8be288b67d06b49d4420fcdf367777019d412bf1
SheldonStringTheory <emirsteveroger@gmail.com>
2022-08-04 00:54:38 +0530

    adding open menu

--- main.py
+++ main.py
@@ -5,13 +5,18 @@ compiler.title('Ezap editor')
 
 def run():
     code = editor.get('1.0', END)
-    print(code)
+    exec(code)
 
 menu_bar = Menu(compiler)
 
 run_bar = Menu(menu_bar, tearoff=0)
 run_bar.add_command(label='Run', command=run)
 menu_bar.add_cascade(label='Run', menu=run_bar)
+
+file_menu = Menu(menu_bar, tearoff=0)
+file_menu.add_command(label='Open', command=run)
+menu_bar.add_cascade(label='File', menu=file_menu)
+
 compiler.config(menu=menu_bar)
 
 editor = Text()
